bf694fa8538eb9d9daf66aae8619e44eae46dc34
Justin Salisi <jsalisi@protonmail.com>
2025-01-23 21:43:53 -0800

    Refactored background set in preparation for dynamic api update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Virtual_Theatre';
+
+  imagePath: string =
+    'https://vignette4.wikia.nocookie.net/club-penguin-rewritten/images/2/2a/Iceberg.png/revision/latest?cb=20170224125550';
+
+  uploadBackgroundImage() {
+    this.imagePath =
+      'https://images.pexels.com/photos/2574997/pexels-photo-2574997.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260';
+  }
 }
