9ab63a8d69b656d2dbaf1a474ec0a088676d9af7
Justin Salisi <jsalisi@protonmail.com>
2025-01-29 14:35:06 -0800

    changed port

--- src/api/startup.sh
+++ src/api/startup.sh
@@ -4,4 +4,4 @@ echo "Using virtual environment named 'virtual-theatre-api'.."
 
 export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/api/antenv/lib/python3.12/site-packages"
 
-gunicorn --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 --timeout 600 --access-logfile '-' --error-logfile '-' api:app
+gunicorn --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:5000 --timeout 600 --access-logfile '-' --error-logfile '-' api:app
