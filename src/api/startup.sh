4d6330303a2f706655a9884e3097b706df94e6f6
Justin Salisi <jsalisi@protonmail.com>
2025-01-29 16:43:20 -0800

    removed startup script

--- src/api/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Using virtual environment named 'virtual-theatre-api'.."
-
-export PYTHONPATH=$PYTHONPATH:"/home/site/wwwroot/api/antenv/lib/python3.12/site-packages"
-
-gunicorn --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:5000 --timeout 600 --access-logfile '-' --error-logfile '-' api:app
