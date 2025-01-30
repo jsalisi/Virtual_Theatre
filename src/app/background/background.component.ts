af7be30fb2cf8678f8c335d01e95539e8e5f4971
Justin Salisi <jsalisi@protonmail.com>
2025-01-29 20:05:08 -0800

    Added toolbar and changed default background

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-background',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   templateUrl: './background.component.html',
   styleUrl: './background.component.css',
 })
 export class BackgroundComponent {
-  backgroundUrl: string | any =
-    'https://static.vecteezy.com/system/resources/previews/020/335/190/non_2x/modern-abstract-grid-background-template-black-and-white-square-grid-design-vector.jpg';
+  backgroundUrl: string | any = null;
 
   constructor(private dataService: DataService) {}
 
