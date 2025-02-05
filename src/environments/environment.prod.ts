6660575afb410d0cce2754ea450edc7f2b17c5a8
Justin Salisi <jsalisi@protonmail.com>
2025-02-04 20:33:56 -0800

    changed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://vtheatre-api.bjfkdxc3gffce0af.canadacentral.azurecontainer.io:3100'
+  baseUrl: 'https://vtheatre-api-bjfkdxc3gffce0af.canadacentral-01.azurewebsites.net'
 };
