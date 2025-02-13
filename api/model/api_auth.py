6f2a225e11b652f376c96456f8d2775a9a9bcfbd
Justin Salisi <jsalisi@protonmail.com>
2025-02-12 17:28:00 -0800

    refactored api call

--- api/model/api_auth.py
+++ api/model/api_auth.py
@@ -1,7 +1,8 @@
-import os
+import os, json
 
-from fastapi import HTTPException, status, Security, FastAPI
+from fastapi import HTTPException, status, Security, UploadFile
 from fastapi.security import APIKeyHeader, APIKeyQuery
+from azure.storage.blob.aio import BlobServiceClient
 
 from dotenv import load_dotenv, dotenv_values
 
@@ -24,3 +25,17 @@ def get_api_key(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid or missing API Key",
     )
+
+async def upload_to_azure(file: UploadFile, file_name: str,file_type:str):
+    blob_service_client = BlobServiceClient.from_connection_string(os.environ.get("CONNECTION_STRING"))
+    async with blob_service_client:
+            container_client = blob_service_client.get_container_client(container="vtheatre-image-storage")
+            try:
+                blob_client = container_client.get_blob_client(file_name)
+                f = await file.read()
+                await blob_client.upload_blob(f, overwrite=True)
+            except Exception as e:
+                print(e)
+                return HTTPException(401, "Something went terribly wrong..")
+
+    return ("{url}/{filename}".format(url=os.environ.get("BASE_IMAGE_URL"), filename=file_name))
