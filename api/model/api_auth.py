b85fda5fcdd6881e50a43862b2fafa224203a34d
Justin Salisi <jsalisi@protonmail.com>
2025-02-12 19:31:00 -0800

    added environment check to api to use managed service account

--- api/model/api_auth.py
+++ api/model/api_auth.py
@@ -2,6 +2,7 @@ import os, json
 
 from fastapi import HTTPException, status, Security, UploadFile
 from fastapi.security import APIKeyHeader, APIKeyQuery
+from azure.identity import DefaultAzureCredential
 from azure.storage.blob.aio import BlobServiceClient
 
 from dotenv import load_dotenv, dotenv_values
@@ -26,16 +27,25 @@ def get_api_key(
         detail="Invalid or missing API Key",
     )
 
-async def upload_to_azure(file: UploadFile, file_name: str,file_type:str):
+async def upload_to_azure(file: UploadFile, file_name: str, file_type: str, is_prod: bool):
+    
     blob_service_client = BlobServiceClient.from_connection_string(os.environ.get("CONNECTION_STRING"))
+    
+    if is_prod:
+        token_credential = DefaultAzureCredential()
+        blob_service_client = BlobServiceClient(
+            account_url = os.environ.get("BASE_IMAGE_URL"),
+            credential = token_credential)
+    
+    container_name = "vtheatre-image-storage"
     async with blob_service_client:
-            container_client = blob_service_client.get_container_client(container="vtheatre-image-storage")
+            container_client = blob_service_client.get_container_client(container_name)
             try:
                 blob_client = container_client.get_blob_client(file_name)
-                f = await file.read()
-                await blob_client.upload_blob(f, overwrite=True)
+                file_to_upload = await file.read()
+                await blob_client.upload_blob(file_to_upload, overwrite=True)
             except Exception as e:
                 print(e)
                 return HTTPException(401, "Something went terribly wrong..")
-
-    return ("{url}/{filename}".format(url=os.environ.get("BASE_IMAGE_URL"), filename=file_name))
+    
+    return (f"{os.environ.get('BASE_IMAGE_URL')}/{container_name}/{file_name}")
